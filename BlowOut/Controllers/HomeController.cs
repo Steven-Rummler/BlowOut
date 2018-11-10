c133e84eb3df79910e2ee24f32c1e5d9ba4a73cc
Steven Rummler <steven.rummler@gmail.com>
2018-11-09 17:44:03 -0700

    Push whole project

--- BlowOut/Controllers/HomeController.cs
+++ BlowOut/Controllers/HomeController.cs
@@ -20,11 +20,63 @@ namespace BlowOut.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Rentals()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        public ActionResult Trumpet()
+        {
+            ViewBag.Image = "/Content/trumpet.jpg";
+            ViewBag.usedCost = "$25";
+            ViewBag.newCost = "$55";
+            ViewBag.Name = "Trumpet";
+            return View("Instrument");
+        }
+
+        public ActionResult Trombone()
+        {
+            ViewBag.Image = "/Content/trombone.jpg";
+            ViewBag.usedCost = "$25";
+            ViewBag.newCost = "$55";
+            ViewBag.Name = "Trombone";
+            return View("Instrument");
+        }
+
+        public ActionResult Tuba()
+        {
+            ViewBag.Image = "/Content/tuba.jpg";
+            ViewBag.usedCost = "$25";
+            ViewBag.newCost = "$55";
+            ViewBag.Name = "Tuba";
+            return View("Instrument");
+        }
+
+        public ActionResult Flute()
+        {
+            ViewBag.Image = "/Content/flute.jpg";
+            ViewBag.usedCost = "$25";
+            ViewBag.newCost = "$55";
+            ViewBag.Name = "Flute";
+            return View("Instrument");
+        }
+
+        public ActionResult Clarinet()
+        {
+            ViewBag.Image = "/Content/clarinet.jpg";
+            ViewBag.usedCost = "$25";
+            ViewBag.newCost = "$55";
+            ViewBag.Name = "Clarinet";
+            return View("Instrument");
+        }
+
+        public ActionResult Saxophone()
+        {
+            ViewBag.Image = "/Content/saxophone.png";
+            ViewBag.usedCost = "$25";
+            ViewBag.newCost = "$55";
+            ViewBag.Name = "Saxophone";
+            return View("Instrument");
         }
     }
 }
